528b0affd50e73bfeb9fe5a61ea4a59cdcca2d88
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-07-26 00:37:49 -0500

    das

--- eshoppcart/src/app/component/partials/search/search.component.ts
+++ eshoppcart/src/app/component/partials/search/search.component.ts
@@ -8,9 +8,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SearchComponent {
   searchTerm = '';
-constructor(activatedRoute:ActivatedRoute,private router:Router){ }
-
-ngOnInit(): void{
-
+constructor(activatedRoute:ActivatedRoute,private router:Router){
+activatedRoute.params.subscribe((params) => {
+if (params.searchTerm) this.searchTerm = params.searchTerm;
+});
 }
+ngOnInit(): void{
+}
+search(term:string):void{
+  if(term)
+  this.router.navigateByUrl('/search/'+ term);
+}
+
 }
